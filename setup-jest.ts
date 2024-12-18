823a0899f87040bd63d5942f5834ed222f079385
Pablo <kovalwaffe@gmail.com>
2024-12-18 21:04:48 +0100

    add Jest Test Framework

---
+++ setup-jest.ts
@@ -0,0 +1,6 @@
+// Importing "setup-jest.js" directly is deprecated. The file "setup-jest.js" will be removed in the future.
+//   Please use "setupZoneTestEnv" function instead. Example:
+// setup-jest.ts
+import { setupZoneTestEnv } from 'jest-preset-angular/setup-env/zone';
+
+setupZoneTestEnv();
